1f5bcda50ef2dd04bea9fee302f2d0edbb9a39bd
HyungjuJang <dacapolife87@gmail.com>
2021-05-08 14:55:40 +0900

    feat : 값타입 임베디드 타입으로 Entity객체에서 공통적으로 사용하는 클래스 - JPA스펙상 기본생성자는 있어야함. 이부분은 값 셋팅시 reflection통하여 값을 설정하기 때문인듯 - 기본생성자는 public protected가 가능하며 접근의 최소화를 위하여 protected로 해줌

---
+++ src/main/java/me/hjjang/jpa/domain/Address.java
@@ -0,0 +1,26 @@
+package me.hjjang.jpa.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Getter @Setter
+@Embeddable
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // JPA스펙상 만든것
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
